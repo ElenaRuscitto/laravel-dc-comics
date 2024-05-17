2e9d39a2507ec8e7a275259b1439536b27f60080
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-17 15:15:44 +0200

    [add] campo test form

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -12,6 +12,31 @@
         <div class="row">
             <h1 class="my-5">Nuovo Comics</h1>
 
+            @php
+                $status = 'test';
+                $title = '';
+                $description = '';
+                $thumb = '';
+                $price = '';
+                $series = '';
+                $sale_date = '';
+                $type = '';
+                $artists = '';
+                $writers = '';
+                if($status === 'test'){
+                    $title = 'Catwoman Vol. 1: Copycats';
+                    $description = "Following her near-miss wedding with Batman, Catwoman hits the streets to expose a copycat who’s pulling heists around Gotham City. As Selina cracks the whip on her former criminal cohorts, she’s attracting unwanted attention from one of Gotham’s most dangerous groups. The mob? Nope. Try the GCPD. And as if the Bat-Bride didn’t have enough problems, don’t miss the debut of an all-new villain determined to make trouble for all nine of Selina’s lives. Collects issues #1-6 of Catwoman’s new series.";
+                    $thumb = 'https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm';
+                    $price = '$16.99';
+                    $series = 'Catwoman';
+                    $sale_date = '2019-04-10';
+                    $type = 'graphic novel';
+                    $artists = 'Fernando Blanco,Joëlle Jones';
+                    $writers = 'Joëlle Jones';
+                }
+
+            @endphp
+
             <form action="{{route('comics.store')}}" method="POST" class="my-5">
 
                 @csrf
